4de9a0323b821545d9921118d67343ede02de28d
jamfly <cocoredju222@gmail.com>
2019-10-21 11:21:30 +0800

    Setup text-question
    Signed-off-by: jamfly <cocoredju222@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,15 @@ const routes: Routes = [
   { path: 'two', component: ScenarioComponent, data: { path: '../assets/gif/2句.gif' } },
   { path: 'three', component: ScenarioComponent, data: { path: '../assets/gif/3句.gif' } },
   { path: 'four', component: ScenarioComponent, data: { path: '../assets/gif/4句.gif' } },
-  { path: '', component: TextQuestionComponent }
+  { path: '',
+    component: TextQuestionComponent,
+    data:
+    {
+      buttonsText: ['可以喔，綁定LINE會員即可整合', '啊..這邊金額無法累積喔'],
+      questionText: '參加官網跟門市抽獎，公告得獎名單那麼長？我哪有美國時間慢慢查！',
+      imagePath: ''
+    }
+  }
 ];
 
 @NgModule({
--- src/app/shared/text-question/text-question.component.ts
+++ src/app/shared/text-question/text-question.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-text-question',
   templateUrl: './text-question.component.html',
   styleUrls: ['./text-question.component.scss']
 })
-export class TextQuestionComponent implements OnInit {
+export class TextQuestionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  questionText: string;
+  buttonsText: [string, string];
+  imagePath: string;
+  private sub: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route
+      .data
+      .subscribe(data => {
+        this.questionText = data.questionText;
+        this.buttonsText = data.buttonsText;
+        this.imagePath = data.imagePath;
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
